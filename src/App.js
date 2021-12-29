823f4f9dd64595b7ed8d08e5863cf8d5ab7bb68f
Sodiq Sanusi <ademilddiq@gmail.com>
2021-12-29 22:39:05 +0100

    Created single game button components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import FirstPage from "./pages/FirstPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FirstPage />
     </div>
   );
 }
---
+++ src/pages/FirstPage/FirstPageStyled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  width: 60%;
+  display: grid;
+  max-width: 700px;
+  grid-template-areas:'
+   paper nil scissors
+   nil rock nil'
+  ;
+`;
